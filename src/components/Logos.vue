0e7f77d05038f451e97d2d5a87289d0314fdb986
Angga <angga.oktaharisetia@gmail.com>
2020-12-29 02:02:49 +0700

    Logos

---
+++ src/components/Logos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Find Best Logo</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Logos',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
